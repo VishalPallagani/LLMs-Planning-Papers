e16201cb4d1d4dad999c577bdf5be74361acf33a
Vishal Pallagani <vishal.pallagani@gmail.com>
2024-03-02 18:29:18 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+export const jsonData = [
+    {
+      "name": "LLMs in APS",
+      "children": [
+        {
+          "name": "Language Translation",
+          "description": "Involves converting natural language into structured planning languages or formats like PDDL and vice-versa, enhancing the interface between human linguistic input andmachine-understandable planning directives.",
+          "children": [
+            { "name": "Paper A", "url": "http://link-to-paper-a.com" },
+            { "name": "Paper B", "url": "http://link-to-paper-b.com" }
+          ]
+        },
+        {
+          "name": "Plan Generation",
+          "description": "Entails the creation of plans or strategies directly by LLMs, focussing on generating actionable sequences or decision-making processes.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Model Construction",
+          "description": "Utilizes LLMs to construct or refine world and domain models essential for accurate and effective planning.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Multi-agent Planning",
+          "description": "Focusses on scenarios involving multiple agents, where LLMs contribute to coordination and cooperative strategy development.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Interactive Planning",
+          "description": "Centers on scenarios requiring iterative feedback or interactive planning with users, external verifiers, or environment, emphasizing the adaptability of LLMs to dynamic inputs.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Heuristics Optimization",
+          "description": "Applies LLMs in optimizing planning processes through refining existing plans or providing heuristic assistance to symbolic planners.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Tool Integration",
+          "description": "Encompasses studies where LLMs act as central orchestrators or coordinators in a tool ecosystem, interfacing with planners, theorem provers, and other systems.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        },
+        {
+          "name": "Brain-inspired Planning",
+          "description": "Covers research focussing on LLM architectures inspired by neurological or cognitive processes, particularly to enhance planning capabilities.",
+          "children": [
+            { "name": "Paper C", "url": "http://link-to-paper-c.com" },
+            { "name": "Paper D", "url": "http://link-to-paper-d.com" }
+          ]
+        }
+      ]
+    }
+  ];
+  
